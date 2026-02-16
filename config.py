f657aaaef2f8c715127de40357decc69c30c6aba
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-02-16 16:32:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
+    # =========================
+    # Telegram
+    # =========================
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     BOT_USERNAME = os.getenv("BOT_USERNAME")
     UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL")
-    DATABASE_URL = os.getenv("DATABASE_URL", "chatfight.db")
 
+    # =========================
+    # MongoDB
+    # =========================
+    MONGO_URI = os.getenv("MONGO_URI")
+
+    # =========================
+    # Validation
+    # =========================
     @staticmethod
     def validate():
         if not Config.BOT_TOKEN:
             raise ValueError("BOT_TOKEN missing in .env")
+
+        if not Config.MONGO_URI:
+            raise ValueError("MONGO_URI missing in .env")
